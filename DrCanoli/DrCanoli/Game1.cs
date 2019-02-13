fa1b2f37bd3dc577bc6f28a45319d37a2af321cd
Liam Guthrie <ljg2980@ad.rit.edu>
2019-02-13 12:08:06 -0500

    Liam's commit

--- DrCanoli/DrCanoli/Game1.cs
+++ DrCanoli/DrCanoli/Game1.cs
@@ -5,7 +5,7 @@ using Microsoft.Xna.Framework.Input;
 namespace DrCanoli
 {
     /// <summary>
-    /// This is the main type for your game. Neat! -Cam
+    /// This is the main type for your game. Neat! -Cam -Liam
     /// </summary>
     public class Game1 : Game
     {
