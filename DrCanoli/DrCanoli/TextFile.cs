821b8a3adda54c39e302141e4922ae86852679a7
Alex Parrotto <parrottoalex@gmail.com>
2019-02-27 12:31:05 -0500

    Added TextFile class that reads level data in from text file

---
+++ DrCanoli/DrCanoli/TextFile.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace DrCanoli
+{
+    class TextFile
+    {
+        private string fileName;
+        public string FileName
+        {
+            get
+            {
+                return fileName;
+            }
+            set
+            {
+                fileName = value;
+            }
+        }
+        private List<int> positionList;
+
+        public TextFile(string fileName)
+        {
+            this.fileName = fileName;
+        }
+
+        public List<int> Read()
+        {
+            StreamReader reader = null;
+            string lineOfText = null;
+            positionList = new List<int>();
+            while((lineOfText = reader.ReadLine()) != null)
+            {
+                positionList.Add(int.Parse(lineOfText));
+            }
+
+            return positionList;
+        }
+    }
+}
