a732d7d4f5e4fc954ab2d4c55fdce19dedc945ce
Alex Parrotto <aap5033@rit.edu>
2019-03-06 12:18:45 -0500

    Fixed bug where Chris always faced left while moving down

--- DrCanoli/DrCanoli/Player.cs
+++ DrCanoli/DrCanoli/Player.cs
@@ -71,7 +71,7 @@ namespace DrCanoli
                         FighterState = FighterState.Move;
                         if (kbState.IsKeyDown(Keys.D))
                             facingRight = true;
-                        else if(kbState.IsKeyDown(Keys.S))
+                        else if(kbState.IsKeyDown(Keys.A))
 							facingRight = false;
                     }
                     if (kbState.IsKeyDown(Keys.Space))	//when Space is pressed
