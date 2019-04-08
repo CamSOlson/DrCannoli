613c8707e57981d9ee08d59014fc46cbfaac3343
Julien Chaulot <jec1862@rit.edu>
2019-04-08 12:27:43 -0400

    Made a few enums for the Boss Class and the new Bullet class, and began the boss' Update method

---
+++ DrCanoli/DrCanoli/Bullet.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+namespace DrCanoli
+{
+    class Bullet
+    {
+        private Texture2D texture;
+        private Rectangle rect;
+        private Direction dir;
+        public Bullet(Texture2D texture, Rectangle rect, Direction dir)
+        {
+            this.texture = texture;
+            this.rect = rect;
+            this.dir = dir;
+        }
+    }
+}
